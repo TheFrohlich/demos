1ee0a58089a7956c83ac2cba6a3e13a2f88efcc8
theFrohlich <theFrohlich@gmail.com>
2017-05-27 12:06:17 +0300

    dasd

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { HashRouter as Router, Route, Link, hashHistory } from 'react-router-dom';
 import './styles.scss';
-import '../node_modules/highlight.js/styles/railscasts.css'
+// import '../node_modules/highlight.js/styles/railscasts.css'
 
 import { AppComponent } from "./components/app.component";
 
